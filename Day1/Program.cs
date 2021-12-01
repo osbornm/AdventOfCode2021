8f0bad8fe697e5df852548a192fe6c7f851b6d7f
Matthew Osborn <matthew@osbornm.com>
2021-12-01 16:35:54 -0600

    day 1 - part 1 and 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Day1 - Part One");
+
+var lines = from line in File.ReadAllLines("input.txt") where !string.IsNullOrEmpty(line) select line.Trim();
+var convertedInput = lines.Select(line => Double.Parse(line)).ToArray();
+
+var PartOnetotalIncreases = 0;
+for (int i = 1; i < convertedInput.Length; i++)
+{
+    var hasIncreased = convertedInput[i] > convertedInput[i - 1];
+    if(hasIncreased)
+    {
+        PartOnetotalIncreases++;
+    }
+    var friendlyDisplay = hasIncreased ? "increased" : "decreased";
+    Console.WriteLine($"{convertedInput[i]} ({friendlyDisplay})");
+}
+
+Console.WriteLine("***********************************************");
+Console.WriteLine($"answer: {PartOnetotalIncreases}");
+Console.WriteLine("***********************************************");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Day1 - Part Two");
+
+var PartTwototalIncreases = 0;
+var previousSum = convertedInput[0] + convertedInput[1] + convertedInput[2];
+for (int i = 1; i < convertedInput.Length-2; i++)
+{
+    var newSum = convertedInput[i] + convertedInput[i + 1] + convertedInput[i + 2];
+    var hasIncreased = newSum > previousSum;
+    if (hasIncreased)
+    {
+        PartTwototalIncreases += 1;
+    }
+    var friendlyDisplay = hasIncreased ? "increased" : "decreased";
+    previousSum = newSum;
+}
+
+Console.WriteLine("***********************************************");
+Console.WriteLine($"answer: {PartTwototalIncreases}");
+Console.WriteLine("***********************************************");
