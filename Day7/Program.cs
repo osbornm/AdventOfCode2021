560480041a38b80d6b90d38f32f2ad352bb139ec
Matthew Osborn <matthew@osbornm.com>
2021-12-07 22:41:51 -0600

    day 7

---
+++ Day7/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Day7");
+
+var crabs = File.ReadAllText("input.txt").Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x));
+
+var bestOption = (position: crabs.Min(), fuel: long.MaxValue);
+
+
+
+for (int current = 0; current <= crabs.Max(); current++)
+{
+    var fuel = 0L;
+
+    foreach (var crab in crabs)
+    {
+        var steps = Math.Abs(crab - current);
+        fuel += ((steps * (steps + 1)) /2);
+    }
+    if(bestOption.fuel > fuel)
+        bestOption = (position: current, fuel: fuel);
+}
+
+
+Console.WriteLine("***********************************************");
+Console.WriteLine($"answer: {bestOption.fuel}");
+Console.WriteLine("***********************************************");
