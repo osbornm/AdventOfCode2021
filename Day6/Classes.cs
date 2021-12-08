5ebff2978d9f1c1ea77354cc30eee11f957eac56
Matthew Osborn <matthew@osbornm.com>
2021-12-07 22:14:55 -0600

    day 6

---
+++ Day6/Classes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Fish
+    {
+        public int CycleTime { get; set; }
+        public Fish(int cycleTimeStart = 8)
+        {
+            CycleTime = cycleTimeStart;
+        }
+
+        public Fish RunCycle()
+        {
+            if(CycleTime > 0)
+            {
+                CycleTime--;
+            } else if(CycleTime == 0)
+            {
+                CycleTime = 6;
+                return new Fish();
+            }
+            return null;
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,52 @@
+﻿using Day6;
+using System.Drawing;
+
+Console.WriteLine("Day6");
+
+var input = File.ReadAllText("input.txt").Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x));
+
+//var numberOfDaysToRun = 256;
+//var Fishes = input.Select(x => new Fish(x)).ToList();
+//for (int i = 0; i < numberOfDaysToRun; i++)
+//{
+//    var newFishes = new List<Fish>();
+//    foreach (var fish in Fishes)
+//    {
+//        var offSpring = fish.RunCycle();
+//        if(offSpring != null)
+//        {
+//            newFishes.Add(offSpring);
+//        }
+//    }
+//    Fishes.AddRange(newFishes);
+//}
+
+
+
+
+var fishes = new long[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+foreach (var starterFish in input)
+{
+    fishes[starterFish]++;
+}
+
+var numberOfDaysToRun = 256;
+for (int day = 0; day < numberOfDaysToRun; day++)
+{
+    var spawningFish = fishes[0];
+    for (int i = 1; i < 9; i++)
+    {
+        fishes[i-1] = fishes[i];
+    }
+
+    fishes[8] = spawningFish; // the new number of fish is equal to the number that spawned
+    fishes[6] += spawningFish; // reset the spawning fish
+}
+
+
+
+
+Console.WriteLine("***********************************************");
+Console.WriteLine($"answer: {fishes.Sum()}");
+Console.WriteLine("***********************************************");
